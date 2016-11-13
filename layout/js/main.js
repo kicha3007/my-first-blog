3f66314a9bc2f80c4d33a5849cbd790dcd32073b
kicha3007 <kicha3007@gmail.com>
2016-11-13 23:26:39 +0500

    My comment tut@

---
+++ layout/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("#owl-one").owlCarousel( {  	    
+	"items": 1,		
+	"nav":true,
+	"loop": true,  
+	"pagination": true,
+	"dots": true
+
+	});
+
+	$("#owl-two").owlCarousel( {  	    
+	"items": 1,	
+	"loop": true,  
+	"pagination": true,
+	"dots": true,
+	"mouseDrag": false
+
+	});
+});
